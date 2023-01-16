a6d192e9ec2ab363aefb7b4e010fad56e8710f62
yoralierdugo <yoraliyamiletverdugofig@gmail.com>
2023-01-16 08:05:00 -0800

    Consumo de api y forEACH

---
+++ forEach.js
@@ -0,0 +1,36 @@
+/* 
+const edades=[22,4,5,8,9];
+
+/*console.log(edades[0]);*/ 
+
+/* edades.forEach((valor) => {
+    console.log(valor);
+}); */  
+
+
+const usuarios =[
+
+    {
+        nombre: 'Yorali',
+        edad: 20,
+        genero:'F'
+
+    },
+
+    {
+        nombre: 'Mario',
+        edad: 28,
+        genero:'M'
+
+    }, 
+
+    {
+        nombre: 'Luis',
+        edad: 18,
+        genero:'M'
+
+    }
+]; 
+usuarios.forEach((usuario) => {
+console.log(usuario.edad);
+});
