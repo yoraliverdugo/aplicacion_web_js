18f5c20d264a80bca89bd8e132e91b78f960e691
yoralierdugo <yoraliyamiletverdugofig@gmail.com>
2023-02-13 08:55:11 -0800

    Imagenes y  mas 13/02/2023

---
+++ SegundoParcial/pokemonact1.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            pokemon: null
+
+        }
+
+
+
+    },
+    methods: {
+        async getpokemon() {
+            // Metodos: 
+            //Post crear
+            //Get obtener
+            //Put modififcar 
+            //Delete eliminar
+            const valor = Math.floor(Math.random() * 100);
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + valor, {
+                    method: 'Get'
+                }); // Consumir la api 
+
+            const datapokemon = await response.json();
+            this.pokemon = datapokemon;
+
+            //console.log(datapokemon.sprites.other.home.front_default)
+
+        }
+
+    }
+
+
+}).mount('#app')
